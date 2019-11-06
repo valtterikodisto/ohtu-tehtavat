b4261f976c57f5083060d0924231b2a2419cb1be
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-11-06 03:12:53 +0200

    Added verkkokauppa

---
+++ viikko2/verkkokauppa/src/main/java/ohtu/AppConfig.java
@@ -0,0 +1,9 @@
+package ohtu;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "ohtu.verkkokauppa")
+public class AppConfig {
+}
---
+++ viikko2/verkkokauppa/src/main/java/ohtu/verkkokauppa/VarastoInterface.java
@@ -0,0 +1,12 @@
+package ohtu.verkkokauppa;
+
+public interface VarastoInterface {
+
+  Tuote haeTuote(int id);
+
+  int saldo(int id);
+
+  void otaVarastosta(Tuote t);
+
+  void palautaVarastoon(Tuote t);
+}
